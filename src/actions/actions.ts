acf4faea3fb407e7c78a5468d7cc5424f266594a
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-04-21 21:34:07 +0530

    fix

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -2,18 +2,14 @@
 import { Ollama } from "@langchain/community/llms/ollama";
 import ollama from "ollama";
 
-export const getData = async () => {
+export const getData = async (question: string) => {
   const model = new Ollama({
     model: "llama3",
   });
 
-  const response = await model.stream(
-    "What is generics in typescript? Explain very briefly"
+  const response = await model.invoke(
+    `You're a sentiment analsis AI, you'll analyse this text ${question} and give detailed sentiment analysis. Don't indulge in unnecessary jargon, be very professional. You'll first categorise the input as either negitive or positive and after that give a very brief explanation for why you think it's negtive or positive`
   );
 
-  const chunks = [];
-  for await (const chunk of response) {
-    chunks.push(chunk);
-    console.log(chunk);
-  }
+  return response;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,37 @@
 "use client";
 import { getData } from "@/actions/actions";
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [text, setText] = useState("");
+  const [question, setQuestion] = useState("");
+  const [loading, setLoading] = useState(false);
   const handleClick = async () => {
-    const res = await getData();
+    setLoading(true);
+    const res = await getData(question);
+
+    setText(res);
+    setLoading(false);
   };
   return (
     <div>
-      <button onClick={handleClick}>Click me</button>
+      <div className="flex flex-col gap-2 w-fit">
+        <input
+          className="border-2"
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+        <button
+          className="w-fit py-2 bg-rose-200 rounded-md px-5"
+          onClick={handleClick}
+        >
+          Click me
+        </button>
+      </div>
+
+      <div>{loading && <h1>Loading...</h1>}</div>
+
+      <pre>{text && text}</pre>
+      {/* {text && <div dangerouslySetInnerHTML={{ __html: text }}></div>} */}
     </div>
   );
 };
